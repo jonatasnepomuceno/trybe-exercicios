a9c7408726dc54eef98e6838bcff25cc07682575
Jonatas Nepomuceno <jonatas.nepo@gmail.com>
2021-10-08 14:56:41 -0300

    Operador Lógico

--- fundamentos/bloco-04-javascript/dia01-introducao/fixar.js
+++ fundamentos/bloco-04-javascript/dia01-introducao/fixar.js
@@ -42,7 +42,7 @@ console.log(area);
 const perimeter = base + heigth + base + heigth;
 console.log(perimeter); */
 
-// If e Else
+/* If e Else
 
 const nota = 30;
 
@@ -54,4 +54,34 @@ if (nota>= 80) {
   console.log("Você foi reprovada(o)");
 } else {
   console.log("Faça o teste");
+} */
+
+// Operadores lógicos
+// AND 
+
+const currentHour = 20;
+let message;
+
+if (currentHour >= 22) {
+  message = "Não deveríamos comer nada, é hora de dormir";
+} else if (currentHour >= 18 && currentHour < 22) {
+  message = "Rango da noite, vamos jantar :D";
+} else if (currentHour >= 14 && currentHour < 18) {
+  message = "Vamos fazer um bolo pro café da tarde?";
+} else if (currentHour >= 11 && currentHour < 14) {
+  message = "Hora do almoço!!!";
+} else if (currentHour >= 4 && currentHour < 11) {
+  message = "Hmmm, cheiro de café recém passado";
+} 
+
+console.log(message);
+
+// OR
+
+let weekDay = "sexta-feira";
+
+if (weekDay === 'segunda-feira' || weekDay === 'terça-feira' || weekDay === 'quarta-feira' || weekDay === 'quinta-feira' || weekDay === 'sexta-feira') {
+  console.log("Oba, mais um dia de aprendizado na Trybe >:D")
+} else {
+  console.log("FINALMENTE, descanso merecido UwU")
 }
