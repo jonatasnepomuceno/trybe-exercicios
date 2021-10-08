88b27c7410405e5117ffaf26bceeb7779ecb81e9
Jonatas Nepomuceno <jonatas.nepo@gmail.com>
2021-10-08 13:32:09 -0300

    Primeiros passos em JavaScript

---
+++ fundamentos/bloco-04-javascript/dia01-introducao/javascript.js
@@ -0,0 +1,19 @@
+//Primeiros passos
+
+const myName = "Jônatas";
+
+const birthCity = "Rio Branco";
+
+const birthYear = 1997;
+
+console.log(myName);
+console.log(birthCity);
+console.log(birthYear);
+
+birthYear = 2030;
+
+console.log(birthYear);
+
+birthCity = "Franca";
+
+console.log(birthCity);
