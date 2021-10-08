fab5b553ed8b7b9270f99a5cd2ea4b4cdaa825f5
Jonatas Nepomuceno <jonatas.nepo@gmail.com>
2021-10-08 13:44:48 -0300

    Tipos primitivos, Operações Aritméticas

--- fundamentos/bloco-04-javascript/dia01-introducao/javascript.js
+++ fundamentos/bloco-04-javascript/dia01-introducao/javascript.js
@@ -1,19 +1,43 @@
 //Primeiros passos
 
-const myName = "Jônatas";
+//const myName = "Jônatas";
 
-const birthCity = "Rio Branco";
+//const birthCity = "Rio Branco";
 
-const birthYear = 1997;
+//const birthYear = 1997;
 
-console.log(myName);
-console.log(birthCity);
-console.log(birthYear);
+//console.log(myName);
+//console.log(birthCity);
+//console.log(birthYear);
 
-birthYear = 2030;
+//birthYear = 2030;
 
-console.log(birthYear);
+//console.log(birthYear);
 
-birthCity = "Franca";
+//birthCity = "Franca";
 
-console.log(birthCity);
+//console.log(birthCity);
+
+// Tipos primitivos, tipagem dinâmica e Operações Aritméticas
+
+let patientId = '50';
+let isEnrolled = true;
+const patientInfo = {
+  firstName: 'Ana',
+  lastName: 'Santos',
+};
+const patientEmail = 'ana@email.com';
+
+console.log(typeof patientId);
+console.log(typeof isEnrolled);
+console.log(typeof patientInfo)
+console.log(typeof patientEmail)
+
+console.log(typeof patientAge) 
+
+const base = 5;
+const heigth = 8;
+const area = base * heigth;
+console.log(area);
+const perimeter = base + heigth + base + heigth;
+console.log(perimeter);
