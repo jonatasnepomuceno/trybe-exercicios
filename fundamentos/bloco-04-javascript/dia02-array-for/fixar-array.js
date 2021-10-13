cc618503dd06a58dc01f6d6c00e709a9d7a3079b
Jonatas Nepomuceno <jonatas.nepo@gmail.com>
2021-10-13 14:16:55 -0300

    Exerciciso Fixar

---
+++ fundamentos/bloco-04-javascript/dia02-array-for/fixar-array.js
@@ -0,0 +1,19 @@
+// Fixar 1
+
+let menu = ['Home', 'Serviços', 'Portfólio', 'Links'];
+let menuServices = menu.length;
+
+console.log(menuServices);
+
+// Fixar 2
+
+//let menu = ['Home', 'Serviços', 'Portfólio', 'Links'];
+let indexOfPortfolio = menu.indexOf('Portfólio');
+
+console.log(indexOfPortfolio);
+
+//Fixar 3
+
+menu.push('Contato'); 
+
+console.log(menu);
---
+++ fundamentos/bloco-04-javascript/dia02-array-for/fixar-for.js
@@ -0,0 +1,16 @@
+// Fixar 1
+
+let groceryList = ['Arroz', 'Feijão', 'Alface', 'Melancia'];
+
+for (let index = 0; index < groceryList.length ; index +=1 ) {
+  console.log(groceryList[index])
+}
+
+
+// Fixar for/of 1 
+
+let names = ['João', 'Maria', 'Antônio', 'Margarida'];
+
+for (let index of names) {
+  console.log(index);
+}
